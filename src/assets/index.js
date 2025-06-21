e8df8a6a8da79f61cd3fc2723dbe92f3066ee058
Manav Panchal <manavpanchal9462@gmail.com>
2025-06-21 11:08:43 +0530

    Remove unnecessary blank line from index.js for improved clarity

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,7 +15,6 @@ import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.jpg";
 
-
 export {
   logo,
   bannerImg,
